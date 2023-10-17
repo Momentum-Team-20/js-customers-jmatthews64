385a83466c5126a11751ee9e3bd08f6721b04d95
jmatthews64 <jon.matthews64@gmail.com>
2023-10-17 14:49:50 -0400

    Added email, and added class creators to all child elements

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
 //Selecting the frame the customer thumbs will be created in.
 const customerContainer = document.querySelector("#mainFrame");
 
-//Create a function to abbreviate states
-// function stateAbrv (stateName) {
-//     for (let state of stateName) {
-//         if(state.name) {
-//             console.log(state.abbreviation);
-//             return state.abbreviation;
-//         }
-//     }
-// }
-
-
 function buildCustomer (customerArray){
     for (let customer of customerArray) {
         //Create a div container for each customer
         let box = document.createElement('div');
         //add class to the box
         box.classList.add("customerThumb");
+        
         //Create a photo box
         customerContainer.appendChild(box);
         console.log(box);
@@ -25,17 +15,28 @@ function buildCustomer (customerArray){
         photo.src = customer.picture.thumbnail;
         photo.alt = "customer thumbnail img";
         box.appendChild(photo);
+        
         //Create Customer Name in box
         let custName = document.createElement('h2');
+        custName.classList.add("name");
         custName.innerText = (customer.name.first + " " 
         + customer.name.last);
         box.appendChild(custName);
+        
+        //Add customer email with a class
+        let custEmail = document.createElement("div");
+        custEmail.classList.add("email");
+        custEmail.innerText = customer.email;
+        box.appendChild(custEmail);
+
         //Add Customer address to box
         let custAddress1 = document.createElement('div');
+        custAddress1.classList.add("address")
         custAddress1.innerText = (customer.location.street.number 
         + " " + customer.location.street.name);
         box.appendChild(custAddress1);
         let custAddress2 = document.createElement("div");
+        custAddress2.classList.add("address");
         custAddress2.innerText = (`${customer.location.city}, ${nameToAbbr(customer.location.state)} ${customer.location.postcode}`);
         box.appendChild(custAddress2);
 
