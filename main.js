e61fdd67b877dbc5aabb461b54b1a0629b4b2f77
jmatthews64 <jon.matthews64@gmail.com>
2023-10-17 12:15:12 -0400

    added code for js to create img and name elements

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("connected");
+//Selecting the frame the customer thumbs will be created in.
+const customerContainer = document.querySelector("#mainFrame");
+
+function buildCustomer (customerArray){
+    for (let customer of customerArray) {
+        //Create a div container for each customer
+        let box = document.createElement('div');
+        //add class to the box
+        box.classList.add("customerThumb");
+        //Create a photo box
+        console.log(box);
+        let photo = document.createElement('img');
+        photo.src = customer.picture.thumbnail;
+        photo.alt = "customer thumbnail img";
+        box.appendChild(photo);
+        console.log(photo);
+        //Create Customer Name in box
+        let custName = document.createElement('h2');
+        custName.innerText = (customer.name.first + " " 
+        + customer.name.last);
+        box.appendChild(custName);
+        console.log(custName);
+    }
+}
+
+buildCustomer(customers);
